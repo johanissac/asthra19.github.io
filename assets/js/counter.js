0bb7d9466ab444636e1dbc6c8ed8070d1055a6c8
Johan Issac <johanissac@gmail.com>
2018-11-30 21:13:35 +0530

    flipclock

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,31 +1,19 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 24 2019 00:00:00").getTime();
+var clock;
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+$(document).ready(function() {
 
-    // Get todays date and time
-    var now = new Date().getTime();
+  // Grab the current date
+  var currentDate = new Date();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+  // Set some date in the future. In this case, it's always Jan 1
+  var futureDate  = new Date(2019,0,24);
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Calculate the difference in seconds between the future and current date
+  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-    // Output the result in an element with id="demo"
-  
-    document.getElementById("days").innerHTML=days;
-    document.getElementById("hours").innerHTML=hours;
-    document.getElementById("minutes").innerHTML=minutes;
-    document.getElementById("seconds").innerHTML=seconds;
-
-    // If the count down is over, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("counter").innerHTML = "EXPIRED";
-    }
-}, 1000);
+  // Instantiate a coutdown FlipClock
+  clock = $('.clock').FlipClock(diff, {
+    clockFace: 'DailyCounter',
+    countdown: true
+  });
+});
